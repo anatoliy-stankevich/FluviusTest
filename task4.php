6386fa25ffb0de147b23ac8b52a0b5fb7a5ee63c
Anatoliy Stankevich <42171415+anatoliy-stankeich@users.noreply.github.com>
2018-08-22 07:50:11 +0300

    Ok
    Время выполнения:
    1. - 15 минут;
    2. - 15 минут;

---
+++ task4.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>4. PHP. Тестовое задание</title>
+</head>
+<body>
+	<h1>4. PHP. Тестовое задание</h1>
+	<h2>Task 1</h2>
+
+	<p>
+		1. Написать функцию нахождения максимального числа в массиве любой вложенности.
+		Не изменять входных аргументов функции.
+	</p>
+	<pre>
+		function myMax($xs) {
+			$min = 0;
+			foreach($xs as $key=>$value){
+				if(is_array($value)){
+					if($min < myMax($value))
+						$min = myMax($value);
+				}else{
+					if($min < $value)
+						$min = $value;
+				}
+			}
+			return $min;
+		}
+		 
+		echo myMax([1, 2, [3, 4]]); // 4
+	</pre>
+<?php
+ 
+	function myMax($xs) {
+		$min = 0;
+		foreach($xs as $key=>$value){
+			if(is_array($value)){
+				if($min < myMax($value))
+					$min = myMax($value);
+			}else{
+				if($min < $value)
+					$min = $value;
+			}
+		}
+		return $min;
+	}
+	 
+	echo myMax([1, 2, [3, 4]]); // 4
+
+?>
+	
+	<h2>Task 2</h2>
+
+	<p>
+		2. Написать функцию для сортировки массива.
+	</p>
+	<pre>
+		$list = [20, 10, -5, 0, 5];
+    
+		function mySort($list) {
+			$result = array();
+			while($list) { 
+				$min = min($list);  
+				$key = array_search($min, $list);  
+				unset($list[$key]);  
+				
+				$result[] = $min;
+			} 
+			$GLOBALS['list'] = $result;
+		}
+		
+		mySort($list);
+		
+		print_r($list); // -5, 0, 5, 10, 20
+	</pre>	
+<?php
+
+    $list = [20, 10, -5, 0, 5];
+    
+    function mySort($list) {
+		$result = array();
+		while($list) { 
+			$min = min($list);  
+			$key = array_search($min, $list);  
+			unset($list[$key]);  
+			
+			$result[] = $min;
+		} 
+		$GLOBALS['list'] = $result;
+    }
+    
+    mySort($list);
+    
+    print_r($list); // -5, 0, 5, 10, 20
+	
+?>
+</body>
+</html>
